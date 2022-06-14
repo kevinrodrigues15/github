3f878eb32d0b67b6bf1298d6f8b314fe9803e8ac
admin <admin@Bcalab3-m18>
2022-06-14 09:33:20 +0530

    first comment

---
+++ CaseToolsExam/src/casetoolsexam/fruits.java
@@ -0,0 +1,18 @@
+package casetoolsexam;
+
+public class fruits {
+
+	private static final String string = null;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+   String color;
+   
+   Fruit_19_125(string color) {
+}
+   {
+      this.color=color;
+	}
+
+}
